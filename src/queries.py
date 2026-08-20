ae655bff8263575abf18ff3355ba7b9e1896e8c6
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-08-20 17:32:03 -0300

    feat: add sales analysis queries

--- src/queries.py
+++ src/queries.py
@@ -9,3 +9,37 @@ connection.execute("PRAGMA foreign_keys = ON")
 
 cursor = connection.cursor()
 
+cursor.execute(""" 
+SELECT 
+    clientes.id,
+    clientes.nome,
+    SUM(vendas.quantidade) as quantidade_comprada
+FROM clientes
+    INNER JOIN vendas
+        ON clientes.id = vendas.cliente_id
+    GROUP BY clientes.id,clientes.nome
+    ORDER BY quantidade_comprada DESC
+""")
+
+maiscompra = cursor.fetchall()
+
+print("Clientes que mais compram:")
+print(maiscompra)
+
+cursor.execute("""
+    SELECT
+        produtos.id,
+        produtos.nome,
+        SUM(vendas.quantidade) as quantidade_vendida,
+        SUM(produtos.valor * vendas.quantidade) as faturamento
+    FROM produtos
+        INNER JOIN vendas
+            ON produtos.id = vendas.produto_id
+        GROUP BY produtos.id,produtos.nome
+        ORDER BY faturamento DESC
+
+""")
+
+maiorfaturamento = cursor.fetchall()
+print("produtos com maior faturamento:")
+print(maiorfaturamento)
